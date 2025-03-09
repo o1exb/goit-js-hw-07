6172388ec0ed5164e6a1346a9697b5d0eb5bf0f3
jt <olex.bro92@gmail.com>
2025-03-09 22:07:05 +0100

    hw7-js

--- js/task-4.js
+++ js/task-4.js
@@ -1,4 +1,6 @@
-document.querySelector('.login-form').addEventListener("submit", function (event) {
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', function (event) {
     event.preventDefault();
 
     let form = event.target;
@@ -6,15 +8,15 @@ document.querySelector('.login-form').addEventListener("submit", function (event
     let password = form.elements.password.value.trim();
 
     if (!email || !password) {
-        alert("All form fields must be filled in");
-        return;
+      alert('All form fields must be filled in');
+      return;
     }
 
     let formData = {
-        email: email;
-        password: password
+      email: email,
+      password: password,
     };
 
     console.log(formData);
     form.reset();
-});
+  });
